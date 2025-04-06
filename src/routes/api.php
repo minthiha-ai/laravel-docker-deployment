5eaf60da98ae0d7afb97db76ad3e50dee572bcf4
MinThiha <51397647+minthiha-ai@users.noreply.github.com>
2025-04-06 12:41:23 +0700

    creating apis

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\DistrictController;
+use App\Http\Controllers\Api\TownshipController;
+use App\Http\Controllers\Api\StateRegionController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/request-otp', [AuthController::class, 'requestOtp']);
+Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::resources([
+        'state-regions' => StateRegionController::class,
+        'districts' => DistrictController::class,
+        'townships' => TownshipController::class
+    ]);
+
+    Route::get('/user/profile', [UserController::class, 'show']);
+    Route::put('/user/profile', [UserController::class, 'update']);
+    Route::get('/user/types', [UserController::class, 'userTypes']);
+    Route::get('/user/location', [UserController::class, 'location']);
+});
