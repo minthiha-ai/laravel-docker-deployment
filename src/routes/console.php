dee018dfb8fd77b535acf89d3eb9ef38322bb17e
MinThiha <51397647+minthiha-ai@users.noreply.github.com>
2025-04-06 14:00:03 +0700

    creating apis

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,30 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\DistrictController;
-use App\Http\Controllers\Api\TownshipController;
-use App\Http\Controllers\Api\StateRegionController;
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/request-otp', [AuthController::class, 'requestOtp']);
-Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:api')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::resources([
-        'state-regions' => StateRegionController::class,
-        'districts' => DistrictController::class,
-        'townships' => TownshipController::class
-    ]);
-
-    Route::get('/user/profile', [UserController::class, 'show']);
-    Route::put('/user/profile', [UserController::class, 'update']);
-    Route::get('/user/types', [UserController::class, 'userTypes']);
-    Route::get('/user/location', [UserController::class, 'location']);
-});
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
