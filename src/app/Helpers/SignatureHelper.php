6bfe206ce7e32996f37e069549341ebd0bf123b2
CinPu95 <cinsuandal@aceplussolutions.com>
2025-05-05 20:40:05 +0630

    [DEVELOP-77] Change multi api system

--- src/app/Helpers/SignatureHelper.php
+++ src/app/Helpers/SignatureHelper.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace App\Helpers;
+
+class SignatureHelper
+{
+    public static function generate($apiKey, $secretKey,$timestamp)
+    {
+        $data= $timestamp.$apiKey;
+        return hash_hmac('sha256', $data, $secretKey);
+    }
+}
